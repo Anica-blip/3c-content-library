2cf9bec50cb7287e49d2eeceed04b7d0b0ebc60b
Anica <3c.innertherapy@gmail.com>
2025-11-07 00:19:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const CONFIG = {
     supabase: {
         url: '', 'https://cgxjqsbrditbteqhdyus.supabase.co'
         anonKey: '', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNneGpxc2JyZGl0YnRlcWhkeXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMTY1ODEsImV4cCI6MjA2NjY5MjU4MX0.xUDy5ic-r52kmRtocdcW8Np9-lczjMZ6YKPXc03rIG4'
-        tableName: 'library_backups'
+        tableName: 'content_public',
+        tableName: 'content_private'
     },
     
     // Cloudflare R2 Configuration
